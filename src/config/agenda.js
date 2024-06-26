350ed13f388bbd948a14a34deb0953c8a9c33cc6
Atul <atulmalviyav2@gmail.com>
2024-06-27 01:46:07 +0530

    run script added

--- src/config/agenda.js
+++ src/config/agenda.js
@@ -1,9 +1,17 @@
 const Agenda = require('agenda');
 require('dotenv').config();
-const mongoConnectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/email_scheduler';
+const mongoConnectionString = process.env.MONGODB_URI;
 
 const agenda = new Agenda({
     db: { address: mongoConnectionString, collection: 'jobs' },
+});
+
+agenda.on('ready', () => {
+    console.log('Agenda started');
+});
+
+agenda.on('error', (error) => {
+    console.error('Agenda connection error:', error);
 });
 
 module.exports = agenda;
