c6996204b139d8197fe367a4d4af285a0359a74b
Atul <atulmalviyav2@gmail.com>
2024-06-26 13:31:37 +0530

    CORS error resolved

--- src/app.js
+++ src/app.js
@@ -3,9 +3,20 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const emailRoutes = require('./routes/emailRoutes');
 const authRoutes = require('./routes/authRoutes');
-require('./config/db'); // Ensure DB connection is established
+const cors = require('cors');
+require('./config/db'); 
 
 const app = express();
+
+const corsOptions = {
+    origin: 'http://localhost:5173',  // Replace with your frontend URL
+    credentials: true,  // Allow cookies and authorization headers
+    methods: ['GET', 'POST', 'PUT', 'DELETE']  // Allow relevant HTTP methods
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+
 
 app.use(bodyParser.json());
 app.use(cookieParser());
--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 require('dotenv').config();
 const app = require('./app');
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+const corsOptions = {
+    origin: 'http://localhost:5173',  // Replace with your frontend URL
+    credentials: true,  // Allow cookies and authorization headers
+    methods: ['GET', 'POST', 'PUT', 'DELETE']  // Allow relevant HTTP methods
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
