19b073dd5809c0cb35cd7cdbd3c31ea0b2154bcb
Atul Malviya <65947031+Atmalviya@users.noreply.github.com>
2024-06-28 18:01:56 +0530

    allowing hosted vercel origin

--- src/server.js
+++ src/server.js
@@ -12,9 +12,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-    origin: 'http://localhost:5173',  // Replace with your frontend URL
-    credentials: true,  // Allow cookies and authorization headers
-    methods: ['GET', 'POST', 'PUT', 'DELETE']  // Allow relevant HTTP methods
+    origin: origin: ['http://localhost:5173', 'https://email-scheduler-fe.vercel.app/'],
+    credentials: true,  
+    methods: ['GET', 'POST', 'PUT', 'DELETE'] 
 };
 
 app.use(cors(corsOptions));
